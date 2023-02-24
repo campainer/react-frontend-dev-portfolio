bc35802f4e6e43d02060e3a6b5c161f133f71581
Verma <ar-anand.verma@in-c02dq37pml7l.intra.rakuten.co.jp>
2023-02-24 14:58:02 +0530

    Updates

---
+++ precache-manifest.d5c83a99ac42e9afb409feebde26328d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c7c3ca71fafd5261b9de93ffdbe2e9",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "613002bdb2a1541e1bb9",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "98c6252a6753228a5054",
+    "url": "/react-frontend-dev-portfolio/static/css/main.cf8d5d3f.chunk.css"
+  },
+  {
+    "revision": "613002bdb2a1541e1bb9",
+    "url": "/react-frontend-dev-portfolio/static/js/2.b25bd1bf.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.b25bd1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c6252a6753228a5054",
+    "url": "/react-frontend-dev-portfolio/static/js/main.14106651.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  },
+  {
+    "revision": "0da16f57dd59eee653f32cefbd9c7478",
+    "url": "/react-frontend-dev-portfolio/static/media/myProfile.0da16f57.jpeg"
+  },
+  {
+    "revision": "7c6f6247633c819aa34a698147433932",
+    "url": "/react-frontend-dev-portfolio/static/media/myProfile.7c6f6247.jpg"
+  },
+  {
+    "revision": "3d1bf2b37cc06486e555d12abf3cb243",
+    "url": "/react-frontend-dev-portfolio/static/media/p1.3d1bf2b3.jpg"
+  },
+  {
+    "revision": "aa769ff3a68406fab4325027694598af",
+    "url": "/react-frontend-dev-portfolio/static/media/p1.aa769ff3.jpg"
+  },
+  {
+    "revision": "af30e142d07754bb1b82fc6c4ceaa63a",
+    "url": "/react-frontend-dev-portfolio/static/media/p1.af30e142.jpg"
+  },
+  {
+    "revision": "cb30c8c18328d7091eb2d43a4af35c5e",
+    "url": "/react-frontend-dev-portfolio/static/media/p1.cb30c8c1.jpg"
+  },
+  {
+    "revision": "2485f817ad580a924f4d8286367b4861",
+    "url": "/react-frontend-dev-portfolio/static/media/p2.2485f817.png"
+  },
+  {
+    "revision": "99b3ce933c8ee566c9f8c138e8223c74",
+    "url": "/react-frontend-dev-portfolio/static/media/p2.99b3ce93.jpg"
+  },
+  {
+    "revision": "be5f4e100c1d07731f928496cf335568",
+    "url": "/react-frontend-dev-portfolio/static/media/p2.be5f4e10.jpg"
+  },
+  {
+    "revision": "dc98c68de43ea913af07526a11ad208c",
+    "url": "/react-frontend-dev-portfolio/static/media/p2.dc98c68d.jpg"
+  },
+  {
+    "revision": "2d055c79fc877608e841b22756c48ba9",
+    "url": "/react-frontend-dev-portfolio/static/media/p3.2d055c79.png"
+  }
+]);
